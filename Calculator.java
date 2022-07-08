205638792b4d6fd8d89d73b49d25316c493593ba
SidheshV <vaity.sidhesh.18it2008@gmail.com>
2022-07-08 12:19:05 +0530

    Calculator demo

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+class Functions{
+	private int a;
+	private int b;
+	private int result;
+
+	public Functions(){
+	}
+
+	public Functions(int a,int b){
+		System.out.println( "Hello this was printed from Through Jenkins build " );
+		this.a=a;
+		this.b=b;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculator [a=" + a + ", b=" + b + "]";
+	}
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public int subtract(int a,int b){
+		return a-b;
+	}
+	
+	public int multiply(int a,int b){
+		return a*b;
+	}
+
+	public int divide(int a,int b){
+		return a/b;
+	}
+
+}
+
+public class Calculator {
+
+	
+public static void main(String args[]){
+	Functions fnc = new Functions();
+	System.out.println("Addition :"+fnc.add(3, 7));
+	System.out.println("Subtraction :"+fnc.subtract(10, 5));
+	System.out.println("Multiplication :"+fnc.multiply(5, 5));
+	System.out.println("Division :"+fnc.divide(10, 5));
+}
+		
+}
